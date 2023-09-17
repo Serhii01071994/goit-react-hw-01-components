56883950d7dbdd353f34fb37d775f800483a92d2
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-09-17 20:22:43 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import user from '../assets/user.json';
+import { Profile } from './Profile/Profile';
+import css from './App.module.css'
+
+console.log(user);
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
